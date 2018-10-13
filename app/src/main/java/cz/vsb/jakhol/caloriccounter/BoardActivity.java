bd7b1c1d82eee574a45249db771c69027a183054
Unknown <jakub967@gmail.com>
2018-10-13 17:31:21 +0200

    Project init
    Empty project init

---
+++ app/src/main/java/cz/vsb/jakhol/caloriccounter/BoardActivity.java
@@ -0,0 +1,13 @@
+package cz.vsb.jakhol.caloriccounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
