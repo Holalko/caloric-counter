fa2070af16d85ca1b4ec2b3d85187868e111c247
Unknown <jakub967@gmail.com>
2018-10-20 18:05:31 +0200

    Overall activity in progress

--- app/src/main/java/cz/vsb/jakhol/caloriccounter/activites/BoardActivity.java
+++ app/src/main/java/cz/vsb/jakhol/caloriccounter/activites/BoardActivity.java
@@ -2,27 +2,49 @@ package cz.vsb.jakhol.caloriccounter.activites;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.view.MenuItem;
 import cz.vsb.jakhol.caloriccounter.R;
 
 public class BoardActivity extends Activity {
 
-    BottomNavigationView bottomNavigation;
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-        bottomNavigation = findViewById(R.id.navigationView);
-        bottomNavigation.inflateMenu(R.menu.navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return false;
-            }
-        });
+
+        initBottomNavigation();
 
     }
+
+    private void initBottomNavigation() {
+        bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.inflateMenu(R.menu.navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(bottomNavigationSelect);
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationSelect = item -> {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.navigation_overall : {
+                return  true;
+//                break;
+            }
+            case R.id.navigation_add : {
+                return  true;
+//                break;
+            }
+            case R.id.navigation_profile: {
+                return  true;
+//                break;
+            }
+            default:
+                return false;
+
+        }
+
+    };
+
 }
